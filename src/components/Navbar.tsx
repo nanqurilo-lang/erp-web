864e42b4aac0e5ba2028e71ba3b43d098f149f3a
nanqurilo-lang <nanqurilo@gmail.com>
2025-12-23 12:44:51 +0530

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -108,12 +108,12 @@ console.log(employee);
         {/* Left block */}
         <div className="flex items-center">
           <div className="flex items-center justify-center bg-[#15173a] h-14 w-64 px-4">
-            {/* <span className="text-white text-2xl font-bold tracking-tight">skavo</span> */}
-  <img
+            <span className="text-white text-2xl font-bold tracking-tight">skavo</span>
+  {/* <img
                src={employee?.profilePictureUrl || "/avatar.png"}
               alt="avatar"
               className="h-full w-full object-cover"
-            />
+            /> */}
 
           </div>
 
